b8c59db1c819021863ed78f0adb833b934efe4e6
user112 <vishnupriya.tv@techversantinfo.com>
2023-05-18 18:13:54 +0530

    added Port

--- hello-world.js
+++ hello-world.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.get('/:id', (req, res) => {
     const id = req.params.id;
     if (id == 1)

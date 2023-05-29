fddf128b4c095bcdbb3a0c80ed77d7e18a81e4cd
user112 <vishnupriya.tv@techversantinfo.com>
2023-05-29 16:23:56 +0530

    Changed files and Response Data

--- actions.js
+++ actions.js
@@ -46,7 +46,7 @@ const addConatctActionConfirm = {
             confirmationMessage: "Are you sure you want to Add this Contact?",
             confirmButtonText: "Yes",
             cancelButtonText: "No",
-            httpMethod: "GET",
+            httpMethod: "POST",
             associatedObjectProperties: [
                 "firstname","lastname","email","phone"
               ],

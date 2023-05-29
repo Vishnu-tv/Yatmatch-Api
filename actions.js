d486c95dc7f307b6d5a24e242c42840009266f55
user112 <vishnupriya.tv@techversantinfo.com>
2023-05-29 14:21:54 +0530

    Changed files and Response Data

--- actions.js
+++ actions.js
@@ -63,7 +63,7 @@ const actionTest ={
             actions: [
                 {
                     type: "ACTION_HOOK",
-                    httpMethod: "POST",
+                    httpMethod: "GET",
                     associatedObjectProperties: [ 
                         "firstname","lastname","email"
                     ],

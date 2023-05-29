99661ca7793d1208f8565ec09060be1244928161
user112 <vishnupriya.tv@techversantinfo.com>
2023-05-29 11:59:31 +0530

    Changed files and Response Data

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.get('/dataFetchUrl',
 
   });
 
-app.get('/addContactForm', (req, res) => {
+app.delete('/addContactForm', (req, res) => {
   const filePath = path.resolve(__dirname, './public', 'contact.html')
   fs.readFile(filePath, 'utf8', function (err, data) {
     if (err) {

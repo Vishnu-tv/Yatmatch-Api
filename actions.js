961a4b7ba45ccb0c33f74e2329ba789b5110271a
user112 <vishnupriya.tv@techversantinfo.com>
2023-05-31 15:49:47 +0530

    Changed files and Response Data

--- actions.js
+++ actions.js
@@ -21,7 +21,7 @@ const subscribeAction = {
 const addYachtAction = {
     
         type: "IFRAME",
-        width: 550,
+        width: 500,
         height: 500, 
         uri: "http://localhost:9500/addyacht",
         label: "Add Yacht",

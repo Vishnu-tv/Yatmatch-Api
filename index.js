125829997022b26a8522b6d8190c6f93ccd75778
user112 <vishnupriya.tv@techversantinfo.com>
2023-06-01 17:42:46 +0530

    Changed files and Response Data

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/dataFetchUrl',
           const exists = JSON.parse(clients).some(el => el.email === userMail);
   
           if (!exists) {
-            res.send(actionTest)
+            res.send(addConatctActionConfirm)
           }
           else {
             res.send(fullList);
